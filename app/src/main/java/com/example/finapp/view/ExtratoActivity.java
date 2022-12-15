7a7705ae071160778f3d48d101b6963baa290367
mateus <imw.desenv@gmail.com>
2022-12-14 21:16:00 -0300

    Criação das activities

--- app/src/main/java/com/example/finapp/view/ExtratoActivity.java
+++ app/src/main/java/com/example/finapp/view/ExtratoActivity.java
@@ -1,14 +1,16 @@
-package com.example.finapp;
+package com.example.finapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.finapp.R;
+
+public class ExtratoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_extrato);
     }
 }
---
+++ app/src/main/java/com/example/finapp/view/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.finapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.finapp.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
