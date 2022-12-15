36a7a601f341f8e9676ad1f341df008372fc3700
mateus <imw.desenv@gmail.com>
2022-12-15 00:00:37 -0300

    Modificações nas telas
    - SplashScreen layout finalizado
    
    - Dashboard e cadastro criados

--- app/src/main/java/com/example/finapp/view/SplashScreenActivity.java
+++ app/src/main/java/com/example/finapp/view/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.example.finapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.finapp.R;
 
@@ -12,5 +14,13 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent dashboard = new Intent(SplashScreenActivity.this, DashboardActivity.class);
+                startActivity(dashboard);
+                finish();
+            }
+        }, 3000);
     }
 }
